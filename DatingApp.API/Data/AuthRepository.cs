3c0b2233ffff5743ba844a480fdac11e99a9dbfc
nazar.mazuryk <Nazar.Mazuryk@stryker.com>
2019-02-22 18:01:32 +0200

    Creating login repository method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 
@@ -15,9 +16,38 @@ namespace DatingApp.API.Data
             _context = context;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> LoginAsync(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         }
 
         public async Task<User> Register(User user, string password)
@@ -43,9 +73,14 @@ namespace DatingApp.API.Data
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
