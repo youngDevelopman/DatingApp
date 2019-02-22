9e29f6f5e43ffa092f45e1d664fd3747f93d57e8
nazar.mazuryk <Nazar.Mazuryk@stryker.com>
2019-02-22 19:03:24 +0200

    creating register method in auth controller

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            // validate requst
+
+            username = username.ToLower();
+
+            if(await _repo.UserExists(username))
+            {
+                return BadRequest();
+            }
+
+            var userToCreate = new User()
+            {
+                Username = username
+            };
+
+            var createUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
